e7a1e8fa5d46a372e95a9fba394416ac5185607e
paulo henrique <p.henriquerm@hotmail.com>
2020-09-01 15:48:23 -0300

    Salvando os dados do usuário logado no localStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,3 +1,5 @@
+import { StorageService } from './storage.service';
+import { LocalUser } from './../../models/local_user';
 import { API_CONFIG } from './../../config/api.config';
 import { HttpClient } from '@angular/common/http';
 import { CredenciaisDTO } from './../../models/credenciais.dto';
@@ -6,7 +8,9 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class AuthService {
 
-    constructor(public http: HttpClient){
+    constructor(
+        public http: HttpClient,
+        public storage: StorageService){
     }
 
     authenticate(creds: CredenciaisDTO){
@@ -17,5 +21,17 @@ export class AuthService {
                 observe: 'response',
                 responseType: 'text'
             });
+    }
+
+    successfulLogin(authorizationValue: string){
+        let tok = authorizationValue.substring(7);
+        let user: LocalUser = {
+            token: tok
+        };
+        this.storage.setLocalUser(user);
+    }
+
+    logout(){
+        this.storage.setLocalUser(null);
     }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,7 +33,7 @@ export class HomePage {
   login(){
     this.auth.authenticate(this.creds)
     .subscribe(Response => {
-      console.log(Response.headers.get("Authorization"));
+      this.auth.successfulLogin(Response.headers.get("Authorization"));
       this.navCtrl.setRoot('CategoriasPage');
     }, 
     error => {});
