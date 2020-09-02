c9fea28ff7765ec895b7da743fd35fdc602ae95f
paulo henrique <p.henriquerm@hotmail.com>
2020-09-02 10:56:47 -0300

    Interceptor para incluir token nas requisições

--- src/app/services/domain/cliente.services.ts
+++ src/app/services/domain/cliente.services.ts
@@ -15,8 +15,7 @@ export class ClienteService {
         let authHeader = new HttpHeaders({'Authorization': 'Bearer ' + token});
 
         return this.http.get<ClienteDTO>(
-            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`, 
-            {'headers': authHeader});
+            `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
     getImageFromBucket(id: string): Observable<any>{
