ff25805cac74376ff26d135f5e0cda24b251ac58
paulo henrique <p.henriquerm@hotmail.com>
2020-09-02 09:48:58 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,3 +1,6 @@
+import { API_CONFIG } from './../../config/api.config';
+import { ClienteService } from './../../app/services/domain/cliente.services';
+import { ClienteDTO } from './../../models/cliente.dto';
 import { StorageService } from './../../app/services/storage.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
@@ -16,19 +19,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  email: string;
+  cliente: ClienteDTO;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public storage: StorageService) {
+    public storage: StorageService,
+    public ClienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
-      this.email = localUser.email;
+      this.ClienteService.findByEmail(localUser.email)
+      .subscribe(response => {
+        this.cliente = response
+        this.getImageIfExists();
+      },
+      error => {})
     }
+  }
+
+  getImageIfExists(){
+    this.ClienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(Response => {
+      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+    },
+    error => {})
   }
 
 }
