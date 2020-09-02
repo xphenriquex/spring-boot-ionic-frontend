18572d293bd7e0a4622aed7adfac5aa5f463b026
paulo henrique <p.henriquerm@hotmail.com>
2020-09-02 14:22:17 -0300

    Criando um FormGroup para controlar o formulário

--- src/pages/singup/singup.module.ts
+++ src/pages/singup/singup.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SingupPage } from './singup';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     SingupPage,
   ],
   imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     IonicPageModule.forChild(SingupPage),
   ],
 })
