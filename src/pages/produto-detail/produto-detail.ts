0c2a896f3db434b356270d19804967b6c752cba0
paulo henrique <p.henriquerm@hotmail.com>
2020-09-03 09:23:42 -0300

    Criando página de detalhes do produto

---
+++ src/pages/produto-detail/produto-detail.ts
@@ -0,0 +1,32 @@
+import { ProdutoDTO } from './../../models/produto.dtp';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ProdutoDetailPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-produto-detail',
+  templateUrl: 'produto-detail.html',
+})
+export class ProdutoDetailPage {
+
+  item: ProdutoDTO;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.item = {
+      id: "1",
+      nome: "Mouse",
+      preco: 89.90
+    }
+  }
+
+}
