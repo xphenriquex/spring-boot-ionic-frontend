5d0e6c523acaf3ccceb3549dcf5922648e7bd18a
paulo henrique <p.henriquerm@hotmail.com>
2020-09-04 10:07:25 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -1,3 +1,4 @@
+import { PedidoService } from './../../services/domain/pedidoService';
 import { ClienteService } from './../../services/domain/cliente.services';
 import { EnderecoDTO } from './../../models/endereco.dto';
 import { ClienteDTO } from './../../models/cliente.dto';
@@ -21,23 +22,24 @@ export class OrderConfirmationPage {
   endereco: EnderecoDTO;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService) {
     this.pedido = this.navParams.get('pedido');
   }
 
   ionViewDidLoad() {
     this.cartItems = this.cartService.getCart().items;
     this.clienteService.findById(this.pedido.cliente.id)
-    .subscribe(response => {
-      this.cliente = response as ClienteDTO;
-      this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
-    },
-    error => {
-      this.navCtrl.setRoot('HomePage');
-    });
+      .subscribe(response => {
+        this.cliente = response as ClienteDTO;
+        this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
+      },
+        error => {
+          this.navCtrl.setRoot('HomePage');
+        });
   }
 
   private findEndereco(id: string, list: EnderecoDTO[]): EnderecoDTO {
@@ -48,4 +50,23 @@ export class OrderConfirmationPage {
   total() {
     return this.cartService.total();
   }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout() {
+    console.log(this.pedido);
+     this.pedidoService.insert(this.pedido)
+       .subscribe(response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      },
+      error => {
+        if(error.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+    });
+  }
+  
 }
